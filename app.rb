965c40ac3294487510e5474ba27f1c8755266b90
Jeremy Raines <jraines@gmail.com>
2015-06-30 17:18:26 -0400

    Add flash messages & layout

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './models'
+require 'bundler/setup'
+require 'rack-flash'
 
 set :database, "sqlite3:ardemo.sqlite3"
 set :sessions, true
+
+enable :sessions
+
+use Rack::Flash, sweep: true
 
 get "/" do
   erb :index
@@ -11,9 +17,10 @@ end
 
 get '/welcome' do
   if current_user
-    "Welcome #{current_user.username}"
+    erb :welcome 
   else
-    "please go back to login and login"
+    flash[:notice] = "Please log in"
+    redirect '/'
   end
 end
 
@@ -25,9 +32,11 @@ post "/login" do
 
   if @user.password == password
     session[:user_id] = @user.id
+    flash[:notice] = "Welcome #{@user.username}!"
     redirect '/welcome'
   else
-    "LOGIN ERROR!!!!!!!"
+    flash[:notice] = "Wrong login info, please try again"
+    redirect '/'
   end
 end
 
