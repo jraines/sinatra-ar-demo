e54b6101d41b507f1f51feb2e43bc3ea646117fe
Jeremy Raines <jraines@gmail.com>
2015-06-30 12:17:57 -0400

    Added email column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630143227) do
+ActiveRecord::Schema.define(version: 20150630161242) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.datetime "birthday"
+    t.string   "email"
   end
 
 end
